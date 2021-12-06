2927b499fa95617ccc3312ac29cd19a505f85d79
MatteoScorcia <matteoscorcia@gmail.com>
2021-12-07 00:18:59 +0100

    adjust script section 2

--- assigment1/section2/execute_pings.sh
+++ assigment1/section2/execute_pings.sh
@@ -5,7 +5,8 @@ for type in core socket node
 	do for i in ob1 ucx
 		 do for j in tcp vader
 			do
-				mpirun  --map-by $type --mca pml $i --mca btl self,$j -np 2 --report-bindings ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 28 2>/dev/null | grep -v ^\# | grep -v '^$' | sed 's/   */,/g' | cut -c 2- > "$type-$i-$j".csv
+				echo "#mpirun  --map-by $type --mca pml $i --mca btl self,$j -np 2 --report-bindings ./IMB-MPI1 PingPong -msglog 28" >> "$type-$i-$j".csv
+				mpirun  --map-by $type --mca pml $i --mca btl self,$j -np 2 --report-bindings ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 28 2>/dev/null | grep -v ^\# | grep -v '^$' | sed 's/   */,/g' | cut -c 2- >> "$type-$i-$j".csv
 				echo "$type-$i-$j"
 			done
 		done
