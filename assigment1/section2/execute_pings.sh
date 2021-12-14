05f20ff48b44256d16c25e72f942f15e0d3dfc9b
MatteoScorcia <matteoscorcia@gmail.com>
2021-12-14 16:53:24 +0100

    modify script

--- assigment1/section2/execute_pings.sh
+++ assigment1/section2/execute_pings.sh
@@ -4,18 +4,19 @@ for type in core socket node
 	do for i in ob1 ucx
 		 do for j in tcp vader
 			do
-				if ["$type-$i-$j" != "node-ob1-vader"]
+				namefile = "$type-$i-$j"
+				if ["$namefile" != "node-ob1-vader"]
 				then
-					echo "mpirun  --map-by $type --mca pml $i --mca btl self,$j -np 2 ./IMB-MPI1 PingPong -msglog 29" > "csv/openmpi/$type-$i-$j".csv
+					echo "mpirun  --map-by $type --mca pml $i --mca btl self,$j -np 2 ./IMB-MPI1 PingPong -msglog 29" > "csv/openmpi/$namefile".csv
 					if ["$type" == "node"]
 					then
-						echo "$1, $2" >> "csv/openmpi/$type-$i-$j".csv
+						echo "$1, $2" >> "csv/openmpi/$namefile".csv
 					else
-						echo "$1" >> "csv/openmpi/$type-$i-$j".csv
+						echo "$1" >> "csv/openmpi/$namefile".csv
 					fi
-					echo "lamba, bandwith computed by fitting data" >> "csv/openmpi/$type-$i-$j".csv
-					mpirun  --map-by $type --mca pml $i --mca btl self,$j -np 2 ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/openmpi/$type-$i-$j".csv
-					echo "$type-$i-$j"
+					echo "lamba, bandwith computed by fitting data" >> "csv/openmpi/$namefile".csv
+					mpirun  --map-by $type --mca pml $i --mca btl self,$j -np 2 ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/openmpi/$namefile".csv
+					echo "$namefile"
 				fi
 			done
 		done
