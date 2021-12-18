1c2152e2fce44095b9afda7c3cd9e201834e87c4
MatteoScorcia <matteoscorcia@gmail.com>
2021-12-18 13:45:44 +0100

    modifiy script

--- assigment1/section2/execute_results.sh
+++ assigment1/section2/execute_results.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-conda activate ml #my virtual environment
+#remember to activate your python environment with all dependecies installed before the run
 
 python process_mean_csv.py
 python fit_and_plot.py
