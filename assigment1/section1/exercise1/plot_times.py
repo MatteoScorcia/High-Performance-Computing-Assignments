edccfb407684976c6556cd4d1ddd417ff94e56a4
MatteoScorcia <matteoscorcia@gmail.com>
2021-12-19 16:27:31 +0100

    refactoring

--- assigment1/section1/exercise1/plot_times.py
+++ assigment1/section1/exercise1/plot_times.py
@@ -8,7 +8,9 @@ if __name__ == '__main__':
     df = pd.read_csv("ring_times.csv")
 
     plt.rcParams["figure.figsize"] = (12, 10)
-    plt.scatter(df['#Processors'], df['MeanTime'], label="time vs #processors")
+    plt.scatter(df['#Processors'], df['MeanTime'] *
+                1000000, label="time vs #processors")
+    plt.xticks(df['#Processors'])
     plt.xlabel("#Processors")
     plt.ylabel("t[usec]")
     plt.savefig("plot_times.jpg")
