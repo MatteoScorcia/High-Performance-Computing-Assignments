54697c58ced7708ad5f4bbc7709d5e44431d77b7
MatteoScorcia <matteoscorcia@gmail.com>
2021-12-19 16:20:49 +0100

    refactoring

--- assigment1/section1/exercise1/get_times.sh
+++ assigment1/section1/exercise1/get_times.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 module load openmpi-4.1.1+gnu-9.3.0
-mpicc ring_time_max_latency.c -o ./build/ring_time_max_latency -lm
+mpicc get_ring_time_max_latency.c -o ./build/get_ring_time_max_latency -lm
 
 END=48
 
 for ((i=1;i<=END;i++));
 do
-	mpirun -np $i ./build/ring_time_max_latency
+	mpirun -np $i ./build/get_ring_time_max_latency
 done
