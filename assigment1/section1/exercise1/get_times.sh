f50dd33057d7089195e2764d9143afdb0fe2aa6d
Matteo Scorcia <matteo@login.hpc>
2021-12-21 00:26:53 +0100

    Add try.sh

--- assigment1/section1/exercise1/get_times.sh
+++ assigment1/section1/exercise1/get_times.sh
@@ -4,10 +4,11 @@ mpicc get_ring_time.c -o ./build/get_ring_time
 
 PROCESSORS=48
 ITERATIONS=50
+CWD="~/High-Performance-Computing-Assignments/assignment1/section1/exercise1/"
 
 for ((i=1;i<=PROCESSORS;i++));
 do for((j=1;j<=ITERATIONS;j++));
 	do
-		mpirun -np $i ./build/get_ring_time $j
+		mpirun -np $i $CWD/build/get_ring_time $j
 	done
 done
