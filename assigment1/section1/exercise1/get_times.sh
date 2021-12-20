a100c44a072c1a2b1c4deaa163b0b3f3e677acb2
MatteoScorcia <matteoscorcia@gmail.com>
2021-12-20 13:32:32 +0100

    refactoring

--- assigment1/section1/exercise1/get_times.sh
+++ assigment1/section1/exercise1/get_times.sh
@@ -2,7 +2,7 @@
 module load openmpi-4.1.1+gnu-9.3.0
 mpicc get_ring_time.c -o ./build/get_ring_time
 
-PROCESSORS=48
+PROCESSORS=4
 ITERATIONS=50
 
 for ((i=1;i<=PROCESSORS;i++));
--- assigment1/section1/exercise1/plot_times.py
+++ assigment1/section1/exercise1/plot_times.py
@@ -5,11 +5,17 @@ from numpy import arange
 from scipy.optimize import curve_fit
 
 if __name__ == '__main__':
-    df = pd.read_csv("ring_times_2.csv")
+    df = pd.read_csv("ring_times.csv")
+
+    statistics = df.groupby(['#Processors'], as_index="False")[
+        'Time'].agg([('average', 'mean'), ('standard_deviation', 'std')])
+    print(statistics)
+    print()
 
     plt.rcParams["figure.figsize"] = (12, 10)
-    plt.scatter(df['#Processors'], df['Time'], label="time vs #processors")
-    plt.xticks(df['#Processors'])
+    plt.scatter(statistics.index, statistics['average'],
+                label="average time vs #processors")
+    plt.xticks(statistics.index)
     plt.xlabel("#Processors")
     plt.ylabel("t[sec]")
     plt.savefig("plot_times.jpg")
