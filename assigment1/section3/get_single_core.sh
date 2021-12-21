50ade6214a869002a81ceef36fb1633963e6e8c1
MatteoScorcia <matteoscorcia@gmail.com>
2021-12-21 10:31:29 +0100

    add jacoby 1 core run

--- assigment1/section3/get_single_core.sh
+++ assigment1/section3/get_single_core.sh
@@ -5,3 +5,4 @@ CWD=/u/dssc/matteo/High-Performance-Computing-Assignments/assigment1/section3/
 
 mpif77 -ffixed-line-length-none $CWD/Jacobi_MPI_vectormode.F -o $CWD/jacoby3D.x
 
+mpirun -np 1 $CWD/jacoby3D.x
