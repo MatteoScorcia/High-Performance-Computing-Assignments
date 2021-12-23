2f4ddc4b691398239a281dc96c69bc7944504778
MatteoScorcia <matteoscorcia@gmail.com>
2021-12-23 11:48:01 +0100

    modify scripts

--- assigment1/section2/execute_pings.sh
+++ assigment1/section2/execute_pings.sh
@@ -11,13 +11,37 @@ do
 		do
 			filename=$type-ob1-$btl
 			mkdir -p "csv/openmpi/$filename"
+
+			echo "mpirun  --map-by $type --mca pml ob1 --mca btl self,$btl -np 2 ./IMB-MPI1 PingPong -msglog 29" > "csv/openmpi/$filename/$filename-$iteration".csv
+
+			if [ "$type" == "node" ];
+			then
+				echo "$1, $2" >> "csv/openmpi/$filename/$filename-$iteration".csv
+			else
+				echo "$1" >> "csv/openmpi/$filename/$filename-$iteration".csv
+			fi
+
+			echo "lamba, bandwith computed by fitting data" >> "csv/openmpi/$filename/$filename-$iteration".csv
 			echo "$filename"
+
 			mpirun  --map-by $type --mca pml ob1 --mca btl self,$btl -np 2 ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/openmpi/$filename/$filename-$iteration".csv
 		done
 
 		filename=$type-ucx
 		mkdir -p "csv/openmpi/$filename"
+
+		echo "mpirun  --map-by $type --mca pml ucx -np 2 ./IMB-MPI1 PingPong -msglog 29 "> "csv/openmpi/$filename/$filename-$iteration".csv
+
+		if [ "$type" == "node" ];
+		then
+			echo "$1, $2" >> "csv/openmpi/$filename/$filename-$iteration".csv
+		else
+			echo "$1" >> "csv/openmpi/$filename/$filename-$iteration".csv
+		fi
+
+		echo "lamba, bandwith computed by fitting data" >> "csv/openmpi/$filename/$filename-$iteration".csv 
 		echo "$filename"
+
 		mpirun  --map-by $type --mca pml ucx -np 2 ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/openmpi/$filename/$filename-$iteration".csv
 	done
 done
--- assigment1/section2/execute_pings_intel.sh
+++ assigment1/section2/execute_pings_intel.sh
@@ -4,65 +4,67 @@
 module load openmpi-4.1.1+gnu-9.3.0
 module load intel
 
-for j in tcp mlx 
+for j in tcp mlx shm 
 		do for iteration in {1..5}
 			do 
 				filename="core-$j"
 				mkdir -p "csv/intel/$filename"
 			
-				echo "mpiexec -n 2 -genv I_MPI_PIN_PROCESSOR_LIST=0,2 -genv I_MPI_OFI_PROVIDER $j ./IMB-MPI1 PingPong -msglog 29" > "csv/intel/$filename/$filename-$iteration".csv
+				echo "mpiexec -n 2 -genv I_MPI_PIN_PROCESSOR_LIST=0,2 -genv I_MPI_FABRICS ofi -genv I_MPI_OFI_PROVIDER $j ./IMB-MPI1 PingPong -msglog 29" > "csv/intel/$filename/$filename-$iteration".csv
 				echo "$1" >> "csv/intel/$filename/$filename-$iteration".csv 
 				echo "lamba, bandwith computed by fitting data" >> "csv/intel/$filename/$filename-$iteration".csv  
-				mpiexec -n 2 -genv I_MPI_PIN_PROCESSOR_LIST=0,2 -genv I_MPI_OFI_PROVIDER $j ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/intel/$filename/$filename-$iteration".csv
+				mpiexec -n 2 -genv I_MPI_PIN_PROCESSOR_LIST=0,2 -genv I_MPI_FABRICS ofi -genv I_MPI_OFI_PROVIDER $j ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/intel/$filename/$filename-$iteration".csv
 				echo "$filename-$iteration" 
 			done
 		done
 
-for j in tcp mlx
+for j in tcp mlx shm
 		do for iteration in {1..5}
 			do 
 				filename="socket-$j"
 				mkdir -p "csv/intel/$filename"
 
-				echo "mpiexec -n 2 -genv I_MPI_PIN_PROCESSOR_LIST=0,1 -genv I_MPI_OFI_PROVIDER $j ./IMB-MPI1 PingPong -msglog 29" > "csv/intel/$filename/$filename-$iteration".csv 
+				echo "mpiexec -n 2 -genv I_MPI_PIN_PROCESSOR_LIST=0,1 -genv I_MPI_FABRICS ofi -genv I_MPI_OFI_PROVIDER $j ./IMB-MPI1 PingPong -msglog 29" > "csv/intel/$filename/$filename-$iteration".csv 
 				echo "$1" >> "csv/intel/$filename/$filename-$iteration".csv 
 				echo "lamba, bandwith computed by fitting data" >> "csv/intel/$filename/$filename-$iteration".csv 
-				mpiexec -n 2 -genv I_MPI_PIN_PROCESSOR_LIST=0,1 -genv I_MPI_OFI_PROVIDER $j ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/intel/$filename/$filename-$iteration".csv 
+				mpiexec -n 2 -genv I_MPI_PIN_PROCESSOR_LIST=0,1 -genv I_MPI_FABRICS ofi -genv I_MPI_OFI_PROVIDER $j ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/intel/$filename/$filename-$iteration".csv 
 				echo "$filename-$iteration"
 			done
 		done
 
-for j in tcp mlx
+for j in tcp mlx shm
 		do for iteration in {1..5}
 			do 
 				filename="node-$j"
 				mkdir -p "csv/intel/$filename"
 			
-				echo "mpiexec -n 2 -ppn 1 -genv I_MPI_OFI_PROVIDER $j ./IMB-MPI1 PingPong -msglog 29" > "csv/intel/$filename/$filename-$iteration".csv 
+				echo "mpiexec -n 2 -ppn 1 -genv I_MPI_FABRICS ofi -genv I_MPI_OFI_PROVIDER $j ./IMB-MPI1 PingPong -msglog 29" > "csv/intel/$filename/$filename-$iteration".csv 
 				echo "$1, $2" >> "csv/intel/$filename/$filename-$iteration".csv 
 				echo "lamba, bandwith computed by fitting data" >> "csv/intel/$filename/$filename-$iteration".csv 
-				mpiexec -n 2 -ppn 1 -genv I_MPI_OFI_PROVIDER $j ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/intel/$filename/$filename-$iteration".csv 
+				mpiexec -n 2 -ppn 1 -genv I_MPI_FABRICS ofi -genv I_MPI_OFI_PROVIDER $j ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/intel/$filename/$filename-$iteration".csv 
 				echo "$filename-$iteration"
 			done
 		done
+# (shm -> shared memory transport, like vader)
+# (mlx -> use ucx)
 
-#by core (shm -> shared memory transport)
-# mpiexec -n 2 -genv I_MPI_PIN_PROCESSOR_LIST=0,2 ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/intel/$filename/$filename-$iteration".csv 
+#by core 
+# mpiexec -n 2 -genv I_MPI_PIN_PROCESSOR_LIST=0,2 -genv I_MPI_FABRICS ofi -genv I_MPI_OFI_PROVIDER mlx ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/intel/$filename/$filename-$iteration".csv 
 
-# mpiexec -n 2 -genv I_MPI_PIN_PROCESSOR_LIST=0,2 -genv I_MPI_OFI_PROVIDER tcp ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/intel/$filename/$filename-$iteration".csv 
+# mpiexec -n 2 -genv I_MPI_PIN_PROCESSOR_LIST=0,2 -genv I_MPI_FABRICS ofi -genv I_MPI_OFI_PROVIDER tcp ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/intel/$filename/$filename-$iteration".csv 
 
-# mpiexec -n 2 -genv I_MPI_PIN_PROCESSOR_LIST=0,2 -genv I_MPI_OFI_PROVIDER shm ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/intel/$filename/$filename-$iteration".csv 
+# mpiexec -n 2 -genv I_MPI_PIN_PROCESSOR_LIST=0,2 -genv I_MPI_FABRICS ofi -genv I_MPI_OFI_PROVIDER shm ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/intel/$filename/$filename-$iteration".csv 
 
 #by socket
-# mpiexec -n 2 -genv I_MPI_PIN_PROCESSOR_LIST=0,1 ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/intel/$filename/$filename-$iteration".csv 
+# mpiexec -n 2 -genv I_MPI_PIN_PROCESSOR_LIST=0,1 -genv I_MPI_FABRICS ofi -genv I_MPI_OFI_PROVIDER mlx ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/intel/$filename/$filename-$iteration".csv 
 
 # mpiexec -n 2 -genv I_MPI_PIN_PROCESSOR_LIST=0,1 -genv I_MPI_OFI_PROVIDER tcp ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/intel/$filename/$filename-$iteration".csv 
 
 # mpiexec -n 2 -genv I_MPI_PIN_PROCESSOR_LIST=0,1 -genv I_MPI_OFI_PROVIDER shm ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/intel/$filename/$filename-$iteration".csv 
 
 #by node
-# mpiexec -n 2 -ppn 1 ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/intel/$filename/$filename-$iteration".csv 
+# mpiexec -n 2 -ppn 1 -genv I_MPI_FABRICS ofi -genv I_MPI_OFI_PROVIDER mlx ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/intel/$filename/$filename-$iteration".csv 
 
-# mpiexec -n 2 -ppn 1 -genv I_MPI_OFI_PROVIDER tcp ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/intel/$filename/$filename-$iteration".csv 
+# mpiexec -n 2 -ppn 1 -genv I_MPI_FABRICS ofi -genv I_MPI_OFI_PROVIDER tcp ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/intel/$filename/$filename-$iteration".csv 
 
-# mpiexec -n 2 -ppn 1 -genv I_MPI_OFI_PROVIDER shm ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/intel/$filename/$filename-$iteration".csv 
+# mpiexec -n 2 -ppn 1 -genv I_MPI_FABRICS ofi -genv I_MPI_OFI_PROVIDER shm ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/intel/$filename/$filename-$iteration".csv 
