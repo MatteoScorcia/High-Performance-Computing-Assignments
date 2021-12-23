d5dff2edf16ed772f4b4610134008de826df9cb6
MatteoScorcia <matteoscorcia@gmail.com>
2021-12-23 11:12:23 +0100

    modify script

--- assigment1/section2/execute_pings.sh
+++ assigment1/section2/execute_pings.sh
@@ -4,27 +4,21 @@
 module load openmpi-4.1.1+gnu-9.3.0
 
 for type in core socket node
-	do for i in ob1 ucx
-		 do for j in tcp vader
-		    do 
-		 		filename="$type-$i-$j"
-		 		mkdir -p "csv/openmpi/$filename"
-				for iteration in {1..5}
-				do
-					if [ "$filename" != "node-ob1-vader" ];
-					then
-						echo "mpirun  --map-by $type --mca pml $i --mca btl self,$j -np 2 ./IMB-MPI1 PingPong -msglog 29" > "csv/openmpi/$filename/$filename-$iteration".csv
-						if [ "$type" == "node" ];
-						then
-							echo "$1, $2" >> "csv/openmpi/$filename/$filename-$iteration".csv
-						else
-							echo "$1" >> "csv/openmpi/$filename/$filename-$iteration".csv
-						fi
-						echo "lamba, bandwith computed by fitting data" >> "csv/openmpi/$filename/$filename-$iteration".csv
-						mpirun  --map-by $type --mca pml $i --mca btl self,$j -np 2 ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/openmpi/$filename/$filename-$iteration".csv
-						echo "$filename-$iteration"
-					fi
-				done
-			done
+do 
+	for iteration in {1..5}
+	do
+		for btl in tcp vader
+		do
+			filename=$type-ob1-$btl
+			mkdir -p "csv/openmpi/$filename"
+			echo "$filename"
+			mpirun  --map-by $type --mca pml ob1 --mca btl self,$btl -np 2 ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/openmpi/$filename/$filename-$iteration".csv
 		done
+
+		filename=$type-ucx
+		mkdir -p "csv/openmpi/$filename"
+		echo "$filename"
+		mpirun  --map-by $type --mca pml ucx -np 2 ~/mpi-bench/mpi-benchmarks/src_c/IMB-MPI1 PingPong -msglog 29 2>/dev/null | grep -v ^\# | grep -v '^$' | tr -s ' ' | sed 's/  */,/g' | cut -c 2- >> "csv/openmpi/$filename/$filename-$iteration".csv
 	done
+done
+
