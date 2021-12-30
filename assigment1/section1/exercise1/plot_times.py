6a8cd4f471027ba7b6bd470782b2335d7f2fc563
MatteoScorcia <matteoscorcia@gmail.com>
2021-12-30 15:04:06 +0100

    process data

--- assigment1/section1/exercise1/plot_times.py
+++ assigment1/section1/exercise1/plot_times.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 def T_comm(message_size, c, bandwidth, num_procs):
-    return (c + (message_size / (bandwidth * 1000000))) * num_procs
+    return (c*2 + (message_size / (bandwidth * 1000000)*2)) * num_procs
 
 
 if __name__ == '__main__':
@@ -12,7 +12,6 @@ if __name__ == '__main__':
 
     statistics = df.groupby(['#Processors'], as_index="False")[
         'Time'].agg([('average', 'mean'), ('standard_deviation', 'std')])
-    print(statistics)
 
     plt.rcParams["figure.figsize"] = (12, 10)
     plt.scatter(statistics.index, statistics['average'],
@@ -22,13 +21,11 @@ if __name__ == '__main__':
     plt.ylabel("t[sec]")
     plt.legend(loc="upper left")
     plt.savefig("plot_ring_times.jpg")
-    # plt.clf()
 
     df = pd.read_csv("runtimes.csv")
 
     statistics = df.groupby(['#Processors'], as_index="False")[
         'Time'].agg([('average', 'mean'), ('standard_deviation', 'std')])
-    print(statistics)
 
     plt.scatter(statistics.index, statistics['average'],
                 label="average runtime")
@@ -36,27 +33,32 @@ if __name__ == '__main__':
     plt.xlabel("#Processors")
     plt.ylabel("t[sec]")
 
-    x = np.arange(1, 13)
-    c = 0.2 / 1000000
-    bandwidth = 6220
-    message_size = 4
-    y = [T_comm(message_size, c, bandwidth, num_proc) * 2 for num_proc in x]
-    plt.plot(x, y, 'k-', label="network model (#Processors <= 12)")
-
-    x = np.arange(13, 25)
+    x = np.arange(1, 25)
     c = 0.41 / 1000000
     bandwidth = 5460
     message_size = 4
-    y = [T_comm(message_size, c, bandwidth, num_proc) * 2 for num_proc in x]
-    plt.plot(x, y, 'k--', label="network model (12 < #Processors <= 24)")
+    y = [T_comm(message_size, c, bandwidth, num_proc) for num_proc in x]
+    plt.plot(x, y, 'k-', label="network model (#Processors <= 24)")
 
     x = np.arange(25, 49)
     c = 0.99 / 1000000
     bandwidth = 12060
     message_size = 4
-    y = [T_comm(message_size, c, bandwidth, num_proc) * 2 for num_proc in x]
-    plt.plot(x, y, 'k-.', label="network model (#Processors > 24)")
+    y = [T_comm(message_size, c, bandwidth, num_proc) for num_proc in x]
+    plt.plot(x, y, 'k--', label="network model (#Processors > 24)")
 
     plt.legend(loc="upper left")
     plt.savefig("plot_times.jpg")
     plt.clf()
+
+    speedup = [statistics['average'].iloc[0] /
+               statistics['average'].iloc[i] for i in np.arange(0, 48)]
+
+    efficiency = [speedup[num_proc] / (num_proc+1)
+                  for num_proc in np.arange(0, 48)]
+
+    plt.plot(np.arange(0, 48), efficiency, label="Speedup Efficiency")
+    plt.xlabel("#Processors")
+    plt.ylabel("Efficiency")
+    plt.legend(loc="upper right")
+    plt.savefig("efficiency.jpg")
