38832d344da5dafb1e890486e585a92f7ab26491
MatteoScorcia <matteoscorcia@gmail.com>
2022-01-28 16:10:10 +0100

    add comparator pqsort

--- assignment2/openMP/build_kdtree.v2.c
+++ assignment2/openMP/build_kdtree.v2.c
@@ -361,13 +361,13 @@ void pqsort(kpoint **data, int start, int end,
       int mid_start = mid - start;
       if (mid_start > 0)
 #pragma omp task default(none) final(mid_start < task_cutoff) mergeable        \
-shared(data) firstprivate(start, mid, comparator) untied
+shared(data) firstprivate(start, mid) untied
         pqsort(data, start, mid, comparator, comparator_insort);
 
       int end_mid = end - (mid + 1);
       if (end_mid)
 #pragma omp task default(none) final(end_mid < task_cutoff) mergeable shared(  \
-    data) firstprivate(mid, end, comparator) untied
+    data) firstprivate(mid, end) untied
         pqsort(data, mid + 1, end, comparator, comparator_insort);
     }
   } break;
