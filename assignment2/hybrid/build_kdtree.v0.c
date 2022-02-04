3e017cadd1c2db72e35efa9c81b5cb711533855f
MatteoScorcia <matteoscorcia@gmail.com>
2022-02-04 14:32:46 +0100

    update

--- assignment2/hybrid/build_kdtree.v0.c
+++ assignment2/hybrid/build_kdtree.v0.c
@@ -155,7 +155,7 @@ int main(int argc, char *argv[]) {
   MPI_Status status;
   MPI_Recv(&recv_len, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
   printf("i am processor %d, received len %d\n", my_rank, recv_len);
-  MPI_Recv(&len2, 1, FLOAT_T, 0, 0, MPI_COMM_WORLD, &status);
+  MPI_Recv(&len2, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
   printf("i am processor %d, received len2 %f\n", my_rank, len2);
 
   // double prova;
@@ -257,7 +257,7 @@ struct kdnode *build_kdtree_until_level_then_scatter(kpoint **dataset_ptrs, floa
       kpoint *chunk = malloc(len * sizeof(kpoint));
       copy_dataset_from_ptrs(chunk, dataset_ptrs, len);
       double len2 = chunk[0].coords[0];
-      MPI_Send(&len2, 1, FLOAT_T, counter, 0, MPI_COMM_WORLD);
+      MPI_Send(&len2, 1, MPI_DOUBLE, counter, 0, MPI_COMM_WORLD);
       // kpoint *chunk = malloc(len * sizeof(kpoint));
       // copy_dataset_from_ptrs(chunk, dataset_ptrs, len);
       // printf("first kpoint sent is (%f,%f)\n", chunk[0].coords[0], chunk[0].coords[1]);
