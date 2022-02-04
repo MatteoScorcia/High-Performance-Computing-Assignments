dc11478525007f9cbff92357d3d18ac34f3c2b4c
MatteoScorcia <matteoscorcia@gmail.com>
2022-02-04 16:44:23 +0100

    update

--- assignment2/hybrid/build_kdtree.v0.c
+++ assignment2/hybrid/build_kdtree.v0.c
@@ -155,8 +155,8 @@ int main(int argc, char *argv[]) {
   MPI_Recv(&recv_len, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
   printf("i am processor %d, received len %d\n", my_rank, recv_len);
 
-  kpoint *recv_dataset = malloc(len * sizeof(float_t));
-  MPI_Recv(recv_dataset, 2 * sizeof(float_t), MPI_BYTE, 0, 0, MPI_COMM_WORLD, &status);
+  kpoint *recv_dataset = malloc(len * sizeof(kpoint));
+  MPI_Recv(recv_dataset, len * sizeof(kpoint), MPI_BYTE, 0, 0, MPI_COMM_WORLD, &status);
   
   printf("i am processor %d, first point received: (%f,%f)\n", my_rank, recv_dataset[0].coords[0], recv_dataset[0].coords[1]);
    
@@ -252,7 +252,7 @@ struct kdnode *build_kdtree_until_level_then_scatter(kpoint **dataset_ptrs, floa
       kpoint *chunk = malloc(len * sizeof(kpoint));
       copy_dataset_from_ptrs(chunk, dataset_ptrs, len);
       printf("first kpoint sent is (%f,%f)\n", chunk[0].coords[0], chunk[0].coords[1]);
-      MPI_Send(chunk, 2 * sizeof(float_t), MPI_BYTE, counter, 0, MPI_COMM_WORLD);
+      MPI_Send(chunk, len * sizeof(kpoint), MPI_BYTE, counter, 0, MPI_COMM_WORLD);
 
       free(chunk);
       counter++;
