e348ed73194d9da842c1b0439668744111e76df7
MatteoScorcia <matteoscorcia@gmail.com>
2022-02-07 14:29:06 +0100

    update

--- assignment2/hybrid/test_send.c
+++ assignment2/hybrid/test_send.c
@@ -75,7 +75,7 @@ int main(int argc, char *argv[])
     #pragma omp declare reduction(minimum : struct Compare : omp_out = omp_in.val < omp_out.val ? omp_in : omp_out)\
                            initializer(omp_priv = {100, 10})
    
-    struct Compare min_distance = {100, 10};
+    struct Compare min_distance = {10000, 10000};
     // min_distance.val = distances[0];
     // min_distance.index = 0;
 
