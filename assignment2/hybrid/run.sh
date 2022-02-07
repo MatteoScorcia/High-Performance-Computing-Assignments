119fb762c49d1432e6c5362b1651dba642dd59d8
MatteoScorcia <matteoscorcia@gmail.com>
2022-02-07 11:00:49 +0100

    update

--- assignment2/hybrid/run.sh
+++ assignment2/hybrid/run.sh
@@ -8,3 +8,11 @@ POINTS=1000
 mpicc build_kdtree.v0.c -fopenmp -lm -w -o build_kdtree.v0
 
 mpirun --map-by node --mca btl ^openib --mca pml ucx -np 2 ./build_kdtree.v0 $POINTS
+
+
+#for version 1
+$NUMTHREADS=8
+
+export OMP_NUM_THREADS=2,$NUMTHREADS
+
+export OMP_PLACES=sockets
--- assignment2/hybrid/test_send.c
+++ assignment2/hybrid/test_send.c
@@ -60,11 +60,10 @@ int main(int argc, char *argv[])
 
   if (my_rank == 0) {
     printf("test parallel for...\n");
-    kpoint res;
     kpoint current_median;
     int current_median_idx;
 
-    #pragma omp parallel for implicit(none) shared(dataset, res, computed_median, current_median) schedule(static)
+    #pragma omp parallel for implicit(none) shared(dataset, computed_median, current_median) schedule(static) proc_bind(close)
     for (int i = 0; i < len; i++) {
       if (abs(dataset[i].coords[0] - computed_median.coords[0]) < abs(dataset[i].coords[0] - current_median.coords[0])) {
         current_median = dataset[i];
