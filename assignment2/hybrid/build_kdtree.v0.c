028daff718bd7392b06ed9bab6318905efdb8b36
MatteoScorcia <matteoscorcia@gmail.com>
2022-02-09 16:26:51 +0100

    update

--- assignment2/hybrid/build_kdtree.v0.c
+++ assignment2/hybrid/build_kdtree.v0.c
@@ -482,7 +482,7 @@ void recv_dataset_from_root_process(int *recv_len, int *recv_axis,
   int mpi_root_process = 0;
   MPI_Status status;
   MPI_Recv(recv_len, 1, MPI_INT, mpi_root_process, 0, MPI_COMM_WORLD, &status);
-
+  printf("%d\n", *recv_len);
   recv_dataset = malloc((*recv_len) * sizeof(kpoint));
   MPI_Recv(recv_dataset, (*recv_len) * sizeof(kpoint), MPI_BYTE, mpi_root_process,
            0, MPI_COMM_WORLD, &status);
