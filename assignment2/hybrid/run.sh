718d31841cb1d61b40ad0af8c422a0b343e23ed0
MatteoScorcia <matteoscorcia@gmail.com>
2022-02-17 19:58:11 +0100

    update

--- assignment2/hybrid/run.sh
+++ assignment2/hybrid/run.sh
@@ -1,18 +1,23 @@
 #! /bin/sh
 
-qsub -l nodes=2:16 -I -q dssc -l walltime=1:00:00
-
-module load openmpi-4.1.1+gbu-9.3.0
+# qsub -l nodes=2:ppn=16 -I -q dssc -l walltime=1:00:00
 
 CWD=/u/dssc/matteo/High-Performance-Computing-Assignments/assigment2/hybrid/
 
-POINTS=1000
-$NUMTHREADS=8
+POINTS=(10000000)
+NUMTHREADS=8
+MPI_PROCS=(1 2 4)
 
 export OMP_NUM_THREADS=$NUMTHREADS
 
 export OMP_PLACES=sockets
 
-mpicc build_kdtree.v0.c -fopenmp -lm -w -o build_kdtree.v0
+module load openmpi-4.1.1+gbu-9.3.0
 
-mpirun --map-by node:PE=$NUMTHREADS --mca btl ^openib --mca pml ucx -np 2 ./build_kdtree.v0 $POINTS
+mpicc $CWD/build_kdtree.v0.c -fopenmp -lm -w -o $CWD/build_kdtree.v0
+
+
+for procs in "${MPI_PROCS[@]}"
+do
+  mpirun --map-by node:PE=$NUMTHREADS --mca btl ^openib --mca pml ucx -np $procs ./build_kdtree.v0 ${POINTS[@]}
+done
