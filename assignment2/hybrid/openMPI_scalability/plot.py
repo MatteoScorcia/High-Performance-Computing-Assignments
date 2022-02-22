f6e236489c0861c7e545fbc2e0ec65f2664f6221
MatteoScorcia <matteoscorcia@gmail.com>
2022-02-22 17:32:36 +0100

    update

--- assignment2/hybrid/openMPI_scalability/plot.py
+++ assignment2/hybrid/openMPI_scalability/plot.py
@@ -3,55 +3,55 @@ import pandas as pd
 
 if __name__ == '__main__':
   df = pd.read_csv("strong_scalability.csv")
-  threads = df['#threads'].values
+  processes = df['#mpi processes'].values
   runtimes = df['total runtime'].values
   speedup = [runtimes[0] / x for x in runtimes]
 
   print(speedup)
 
-  plt.scatter(threads, speedup, label='strong scaling')
-  plt.plot(threads, speedup)
-  plt.plot(threads, threads, label='linear speedup')
+  plt.scatter(processes, speedup, label='strong scaling')
+  plt.plot(processes, speedup)
+  plt.plot(processes, processes, label='linear speedup')
   plt.ylabel('speedup')
-  plt.xlabel('#threads')
+  plt.xlabel('#mpi processes')
   plt.legend(loc="upper left")
   plt.savefig("strong_scaling_plot.jpeg")
   plt.clf()
 
-  efficiency = [speedup[i]/threads[i] for i in range(0,len(threads))]
-  plt.scatter(threads, efficiency, label='strong efficiency')
-  plt.plot(threads, efficiency)
-  plt.plot(threads, [1 for _ in threads], label='ideal strong efficiency')
+  efficiency = [speedup[i]/processes[i] for i in range(0,len(processes))]
+  plt.scatter(processes, efficiency, label='strong efficiency')
+  plt.plot(processes, efficiency)
+  plt.plot(processes, [1 for _ in processes], label='ideal strong efficiency')
   plt.ylabel('efficiency')
-  plt.xlabel('#threads')
+  plt.xlabel('#mpi processes')
   plt.legend(loc="lower left")
   plt.savefig("strong_efficiency_plot.jpeg")
   plt.clf()
 
   df = pd.read_csv("weak_scalability.csv")
-  threads = df['#threads'].values
+  processes = df['#mpi processes'].values
   runtimes = df['total runtime'].values
   speedup = []
-  for counter in range(1,10,2):
+  for counter in range(1,6,2):
     speedup.append(runtimes[counter] / runtimes[counter -1])
 
   print(speedup)
 
-  efficiency = [speedup[i]/threads[::2][i] for i in range(0,len(threads[::2]))]
-  plt.scatter(threads[::2], efficiency, label='weak efficiency')
-  plt.plot(threads[::2], efficiency)
-  plt.plot(threads[::2], [1 for _ in threads[::2]], label='ideal weak efficiency')
+  efficiency = [speedup[i]/processes[::2][i] for i in range(0,len(processes[::2]))]
+  plt.scatter(processes[::2], efficiency, label='weak efficiency')
+  plt.plot(processes[::2], efficiency)
+  plt.plot(processes[::2], [1 for _ in processes[::2]], label='ideal weak efficiency')
   plt.ylabel('efficiency')
-  plt.xlabel('#threads')
+  plt.xlabel('#mpi processes')
   plt.legend(loc="lower left")
   plt.savefig("weak_efficiency_plot.jpeg")
   plt.clf()
 
-  plt.scatter(threads[::2], speedup, label='weak scaling')
-  plt.plot(threads[::2], speedup)
-  plt.plot(threads[::2], threads[::2], label='linear speedup')
+  plt.scatter(processes[::2], speedup, label='weak scaling')
+  plt.plot(processes[::2], speedup)
+  plt.plot(processes[::2], processes[::2], label='linear speedup')
   plt.ylabel('speedup')
-  plt.xlabel('#threads')
+  plt.xlabel('#mpi processes')
   plt.legend(loc="upper left")
   plt.savefig("weak_scaling_plot.jpeg")
   plt.clf()
